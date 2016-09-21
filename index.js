97cbf78e7949716b273e039f074104c6ccfe16f6
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2016-09-21 23:49:22 +0300

    Make it listen to any messages and answer it

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var TelegramBot = require('node-telegram-bot-api');
+var addTrollingMessages = require('./lib/trolling-messages');
 //NOTE: You should get this file from one of our team
 var BOT_API_TOKEN = require('./BOT_API_KEY.json').key;
 
 const bot = new TelegramBot(BOT_API_TOKEN, {polling: true});
 
+console.log('Authorizing and initializing the bot...');
+bot.getMe().then(me => {
+    console.log('Bot has been successfully authorized. Name is', me.first_name);
+});
+
+addTrollingMessages(bot);
+
 // Any kind of message
 bot.on('message', msg => {
-    var chatId = msg.chat.id;
-
-    bot.sendMessage(chatId, 'Hello!');
+    console.log('Message received|', msg.from.username, msg.text);
+    if (msg.text.indexOf('Гена') !== -1) {
+        bot.sendMessage(msg.chat.id, `Отстань от меня, ${msg.from.username}!!`);
+    }
 });
