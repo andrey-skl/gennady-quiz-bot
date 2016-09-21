19f4cfafbe43144ff36ce8b0f727c690ac7d0a3b
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2016-09-22 00:58:13 +0300

    Parse and load questions (from test database at the moment)

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-var TelegramBot = require('node-telegram-bot-api');
-var addTrollingMessages = require('./lib/trolling-messages');
+const TelegramBot = require('node-telegram-bot-api');
+const questionsDatabase = require('./lib/questions-database');
+const addTrollingMessages = require('./lib/trolling-messages');
+
+
 //NOTE: You should get this file from one of our team
-var BOT_API_TOKEN = require('./BOT_API_KEY.json').key;
+const BOT_API_TOKEN = require('./BOT_API_KEY.json').key;
 
 const bot = new TelegramBot(BOT_API_TOKEN, {polling: true});
 
 console.log('Authorizing and initializing the bot...');
-bot.getMe().then(me => {
-    console.log('Bot has been successfully authorized. Name is', me.first_name);
-});
 
-addTrollingMessages(bot);
+bot.getMe()
+    .then(me => {
+        console.log('Bot has been successfully authorized. Name is', me.first_name);
 
-bot.on('text', msg => {
-    console.log('Message received|', msg.from.username, msg.text);
-    if (msg.text.indexOf('Гена') !== -1) {
-        bot.sendMessage(msg.chat.id, `Отстань от меня, ${msg.from.first_name}!!`);
-    }
-});
+        addTrollingMessages(bot);
+        bot.on('text', msg => console.log('Message received|', msg.from.username, msg.text));
+
+        return questionsDatabase('./questions-database/questions-test.txt');
+    })
+    .then(questions => {
+        console.log('Questions has been loaded', questions);
+    });
