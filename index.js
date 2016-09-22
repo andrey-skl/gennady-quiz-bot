b6ed9ea772f693a0b2f175674a0068f32c8074a2
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2016-09-22 21:01:45 +0300

    Extract command interface

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const TelegramBot = require('node-telegram-bot-api');
 const questionsDatabase = require('./lib/questions-database');
 const addTrollingMessages = require('./lib/trolling-messages');
 const botMessagesLogger = require('./lib/bot-messages-logger');
-const Game = require('./lib/the-game');
+const commandInterface = require('./lib/command-interface');
 
 const isDev = process.argv.indexOf('--prod') === -1;
 const config = isDev ? require('./config-dev.json') : require('./config-prod.json');
-
-const games = {};
 
 //NOTE: You should get this file from one of our team
 const BOT_API_TOKEN = require('./BOT_API_KEY.json').key;
@@ -27,28 +25,5 @@ bot.getMe()
   .then(questions => {
     console.log('Questions have been loaded, there are ', questions.length, 'questions.');
 
-    bot.on('text', msg => {
-      let game = games[msg.chat.id];
-      if (!game) {
-        game = games[msg.chat.id] = new Game(bot, msg.chat, questions);
-      }
-
-      game.setCurrentPlayer(msg.from);
-
-      if ([Game.STATUS.NEW, Game.STATUS.STOPPED].includes(game.status)) {
-        if (['/start', '.ыефке'].includes(msg.text)) {
-          game.start(10);
-        }
-      } else if ([Game.STATUS.INPROGRESS].includes(game.status)) {
-        if (['/stop', '.ыещз'].includes(msg.text)) {
-          game.stop();
-          game.destroy();
-          games[msg.chat.id] = null;
-        } else if (['/skip', '.ылшз'].includes(msg.text)) {
-          game.skip();
-        } else {
-          game.onAnswer(msg);
-        }
-      }
-    });
+    bot.on('text', msg => commandInterface.reactOnMessage(bot, questions, msg));
   });
