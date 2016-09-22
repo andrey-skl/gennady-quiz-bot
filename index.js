cd000ade1dfaf166f9248a7cd52fb1779bc778e5
Max Maximov <max.maximov@gmail.com>
2016-09-22 05:03:08 +0300

    😘

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ const addTrollingMessages = require('./lib/trolling-messages');
 const botMessagesLogger = require('./lib/bot-messages-logger');
 const Game = require('./lib/Game');
 
-//NOTE: You should get this file from one of our team
-const BOT_API_TOKEN = require('./BOT_API_KEY.json').key;
-
-const bot = new TelegramBot(BOT_API_TOKEN, {polling: true});
-
-console.log('Authorizing and initializing the bot...');
-
 const games = {};
 
 const jjj = {
@@ -25,40 +18,47 @@ const jjj = {
   'vandrianova ': 'Лера'
 };
 
+//NOTE: You should get this file from one of our team
+const BOT_API_TOKEN = require('./BOT_API_KEY.json').key;
+
+const bot = new TelegramBot(BOT_API_TOKEN, {polling: true});
+
+console.log('Authorizing and initializing the bot...');
+
 bot.getMe()
-    .then(me => {
-        console.log('Bot has been successfully authorized. Name is', me.first_name);
-        addTrollingMessages(bot);
-        botMessagesLogger(bot);
+  .then(me => {
+    console.log('Bot has been successfully authorized. Name is', me.first_name);
+    addTrollingMessages(bot);
+    botMessagesLogger(bot);
 
-        return questionsDatabase('./questions-database/questions-test.txt');
-    })
-    .then(questions => {
-        console.log('Questions have been loaded, there are ', questions.length, 'questions.');
+    return questionsDatabase('./questions-database/questions-test.txt');
+  })
+  .then(questions => {
+    console.log('Questions have been loaded, there are ', questions.length, 'questions.');
 
-        bot.on('text', msg => {
-            if (msg.from.username in jjj) {
-              msg.from.jjj = jjj[msg.from.username];
-            } else {
-              msg.from.jjj = msg.from.first_name;
-            }
+    bot.on('text', msg => {
+      if (msg.from.username in jjj) {
+        msg.from.jjj = jjj[msg.from.username];
+      } else {
+        msg.from.jjj = msg.from.first_name;
+      }
 
-            let game = games[msg.chat.id];
+      let game = games[msg.chat.id];
 
-            if (!game) {
-              game = games[msg.chat.id] = new Game(bot, msg.chat, questions);
-            }
+      if (!game) {
+        game = games[msg.chat.id] = new Game(bot, msg.chat, questions);
+      }
 
-            if (['new', 'stopped'].includes(game.status)) {
-              if (msg.text === '/start') {
-                game.start(msg.from);
-              }
-            } else if (['inprogress'].includes(game.status)) {
-              if (msg.text === '/stop') {
-                game.stop(msg.from);
-              } else {
-                game.onAnswer(msg);
-              }
-            }
-        });
+      if (['new', 'stopped'].includes(game.status)) {
+        if (msg.text === '/start') {
+          game.start(msg.from);
+        }
+      } else if (['inprogress'].includes(game.status)) {
+        if (msg.text === '/stop') {
+          game.stop(msg.from);
+        } else {
+          game.onAnswer(msg);
+        }
+      }
     });
+  });
