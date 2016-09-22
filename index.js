951a8c348829eb917467e59862789921f3e01c47
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-09-22 14:24:18 +0300

    Some refactoring and right answer confirmation improves

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ bot.getMe()
 
       game.setCurrentPlayer(msg.from);
 
-      if (['new', 'stopped'].includes(game.status)) {
+      if ([Game.STATUS.NEW, Game.STATUS.STOPPED].includes(game.status)) {
         if (['/start', '.ыефке'].includes(msg.text)) {
           game.start(10);
         }
-      } else if (['inprogress'].includes(game.status)) {
+      } else if ([Game.STATUS.INPROGRESS].includes(game.status)) {
         if (['/stop', '.ыещз'].includes(msg.text)) {
           game.stop();
         } else if (['/skip', '.ылшз'].includes(msg.text)) {
